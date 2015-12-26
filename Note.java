70566885bd9848d65cc0a2b1044cc806f72d7968
mattixpet <matthias01123@gmail.com>
2015-12-26 17:39:17 +0000

    beautify to get rid of indent

--- Note.java
+++ Note.java
@@ -1,51 +1,55 @@
 public class Note {
 
-    private final String note;
-    private final String waveform;
-    private final double hz;
-    private double[] NoteArray;
+  private final String note;
+  private final String waveform;
+  private final double hz;
+  private double[] NoteArray;
 
-    public Note(String note0, String waveform0) 
-    { note = note0; waveform = waveform0; hz = Audio.freq(note); }
+  public Note(String note0, String waveform0) {
+    note = note0;
+    waveform = waveform0;
+    hz = Audio.freq(note);
+  }
 
-    public void play(double t)
-    {
-	if (waveform.equals("Sine")) 
-	    { NoteArray = Audio.tone(hz,t); }
-	else if (waveform.equals("Square")) 
-	    { NoteArray = Audio.square(hz,t); }
-	else if (waveform.equals("Sawtooth"))
-	    { NoteArray = Audio.sawtooth(hz,t); }
-	else if (waveform.equals("Triangle"))
-	    { NoteArray = Audio.triangle(hz,t); }
-	else { NoteArray = new double[1]; } 
-
-	StdAudio.play(NoteArray);
+  public void play(double t) {
+    if (waveform.equals("Sine")) {
+      NoteArray = Audio.tone(hz, t);
+    } else if (waveform.equals("Square")) {
+      NoteArray = Audio.square(hz, t);
+    } else if (waveform.equals("Sawtooth")) {
+      NoteArray = Audio.sawtooth(hz, t);
+    } else if (waveform.equals("Triangle")) {
+      NoteArray = Audio.triangle(hz, t);
+    } else {
+      NoteArray = new double[1];
     }
 
-    public String getWaveform() {
-	return waveform;
-    }
+    StdAudio.play(NoteArray);
+  }
 
-    public String getNote() {
-	return note;
-    }
-	
-	public double getHz() {
-	return hz;
-	}
+  public String getWaveform() {
+    return waveform;
+  }
 
-    public String toString() 
-    {       String wavef = waveform.substring(0,3);
-	    return note + "(" + wavef + ")"; }
+  public String getNote() {
+    return note;
+  }
+
+  public double getHz() {
+    return hz;
+  }
+
+  public String toString() {
+    String wavef = waveform.substring(0, 3);
+    return note + "(" + wavef + ")";
+  }
 
 
 
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-	Note C = new Note("C","Square");
-        C.play(1.0);
-    }
+    Note C = new Note("C", "Square");
+    C.play(1.0);
+  }
 }
-
